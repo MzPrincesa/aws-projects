75a6f12eafcac67eb2eaddad88242bf80f694ee3
Princess Chinwendu Ugwokaegbe <115846148+MzPrincesa@users.noreply.github.com>
2026-08-21 23:47:10 +0000

    new

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+echo ">>> Starting post-create setup"
+
 sudo apt-get update
 sudo apt-get install -y tree
 
+echo ">>> Loading NVM"
+export NVM_DIR="/home/codespace/nvm"
+source "$NVM_DIR/nvm.sh"
+
+echo ">>> Installing Node 24.14.0"
 nvm install 24.14.0
 nvm alias default 24.14.0
+nvm use 24.14.0
+
+echo ">>> post-create setup complete"
